c4f1ee55cfb7fecbe17671bdceaa72883086f086
pforret <pforret@users.noreply.github.com>
2023-03-13 09:31:58 +0000

    Fix styling

--- src/LaraMarkdownController.php
+++ src/LaraMarkdownController.php
@@ -4,18 +4,19 @@ namespace Pforret\LaraMarkdownController;
 
 abstract class LaraMarkdownController
 {
-	public function index(){
-	}
+    public function index()
+    {
+    }
 
-	public function show(string $slug){
-	}
+    public function show(string $slug)
+    {
+    }
 
-	protected function setViewsRoot(string $folder): void
-	{
-	}
+    protected function setViewsRoot(string $folder): void
+    {
+    }
 
-	protected function setMarkdownRoot(string $folder): void
-	{
-	}
-
+    protected function setMarkdownRoot(string $folder): void
+    {
+    }
 }
